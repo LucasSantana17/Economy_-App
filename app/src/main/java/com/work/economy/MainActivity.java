9891a8e27356c5d97eb20d14878e790961b88b89
Joao Lucas <sjoaolucascostasantana@gmail.com>
2024-12-08 22:11:36 -0300

    Atualizações

--- app/src/main/java/com/work/economy/MainActivity.java
+++ app/src/main/java/com/work/economy/MainActivity.java
@@ -3,6 +3,7 @@ package com.work.economy;
 import com.work.economy.Piggybank;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -49,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
                 String item = adapterView.getItemAtPosition(position).toString();
-                Toast.makeText(MainActivity.this, "Item selecionado: ",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Item selecionado! ",Toast.LENGTH_SHORT).show();
 
             }
 
@@ -69,30 +70,45 @@ public class MainActivity extends AppCompatActivity {
                 String name_value = nameValue.getText().toString();
                 String spinner = spinnerSelect.getSelectedItem().toString();
 
-                Log.d(null,"Value" + spinner);
-
                 // Condicional no spinner para verificar a entrada, essa verificação retira o bug
 
                 if(spinner.equals("Nenhum")){
-
                     Toast.makeText(MainActivity.this, "Insira o tipo do valor!!",Toast.LENGTH_SHORT).show();
 
-                }else if(spinner.equals("Adicional")) {
-                    pinggy.accountEntry(name_value, Double.parseDouble(input_Value), typeValue = true );
-                    pinggy.setNameValue(name_value);
-                    pinggy.setInputValue(Double.parseDouble(input_Value));
-                    pinggy.setTypeValue(typeValue = true);
+                }else if(spinner.equals("Adicional")) { // Ajustar a condição para não receber valor nulo nos campos VALOR/NOME
 
-                    nameValue.setText("");
-                    inputValue.setText("");
-                }else if(spinner.equals("Gasto")){
-                    pinggy.accountEntry(name_value, Double.parseDouble(input_Value), typeValue = true );
-                    pinggy.setNameValue(name_value);
-                    pinggy.setInputValue(Double.parseDouble(input_Value));
-                    pinggy.setTypeValue(typeValue = false);
+                    if(input_Value.equals("") && name_value.equals("")){
+                        Toast.makeText(MainActivity.this, "Insira todas as informações !!",Toast.LENGTH_SHORT).show();
+                    }else if(input_Value.equals("") || name_value.equals("")){
+                        Toast.makeText(MainActivity.this, "Insira todas as informações !!",Toast.LENGTH_SHORT).show();
+                    }else {
+                        pinggy.accountEntry(name_value, Double.parseDouble(input_Value), typeValue = true );
+                        pinggy.setNameValue(name_value);
+                        pinggy.setInputValue(Double.parseDouble(input_Value));
+                        pinggy.setTypeValue(typeValue = true);
+                        nameValue.setText("");
+                        inputValue.setText("");
+                        Toast.makeText(MainActivity.this, "Valor iserido com sucesso !!",Toast.LENGTH_SHORT).show();
+                    }
 
-                    nameValue.setText("");
-                    inputValue.setText("");
+
+                }else if(spinner.equals("Gasto")){// Ajustar a condição para não receber valor nulo nos campos VALOR/NOME
+
+                    if(input_Value.equals("") && name_value.equals("")){
+                        Toast.makeText(MainActivity.this, "Insira todas as informações !!",Toast.LENGTH_SHORT).show();
+                    }else if(input_Value.equals("") || name_value.equals("")){
+                        Toast.makeText(MainActivity.this, "Insira todas as informações !!",Toast.LENGTH_SHORT).show();
+                    }else {
+                        pinggy.accountEntry(name_value, Double.parseDouble(input_Value), typeValue = true );
+                        pinggy.setNameValue(name_value);
+                        pinggy.setInputValue(Double.parseDouble(input_Value));
+                        pinggy.setTypeValue(typeValue = false);
+                        nameValue.setText("");
+                        inputValue.setText("");
+                        Toast.makeText(MainActivity.this, "Atualização feita com sucesso !!",Toast.LENGTH_SHORT).show();
+                    }
+
+
                 }
 
 
